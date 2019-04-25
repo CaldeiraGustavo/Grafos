8170530d062e46ee23335ac7f59e8e5ca804eb87
sarahrolandc <sarahrolandc@outlook.com>
2019-04-25 18:50:28 -0300

    modificações

--- Grafos/Grafos/Grafo.cs
+++ Grafos/Grafos/Grafo.cs
@@ -7,11 +7,17 @@ using System.Threading.Tasks;
 namespace Grafos
 {
     public abstract class Grafo
-    {
+    {       
+        private List<Vertice> vertices;
 
         public Grafo()
-        {
+        {          
+              
+        }
 
+        public void addVertice (Vertice vertice)
+        {
+            this.vertices.Add(vertice);
         }
 
         public abstract bool isAdjacente(Vertice v1, Vertice v2);
@@ -26,21 +32,129 @@ namespace Grafos
 
         public abstract bool isNulo();
 
-        public abstract bool isCompleto();
+        //Sarah
+        public bool isCompleto()
+        {
+            int numeroVertices = vertices.Count();
 
-        public abstract bool isConexo();
+            foreach (Vertice vertice in vertices)
+            {
+                if (getGrau(vertice) < numeroVertices - 1)
+                {
+                    return false;
+                }
+            }
 
-        public abstract bool isEuleriano();
+            return true;
+        }
 
-        public abstract bool isUnicursal();
+        //Sarah
+        public bool isConexo()
+        {
+            int componentes = 1;
 
-        public abstract Grafo getComplementar();
+            //Faz uma busca em profundidade para descobrir a quantidade de componentes conexos;
+            foreach (Vertice vertice in vertices)
+            {
+                if (vertice.cor == "BRANCO")
+                {
+                    Visitar(vertice);
+                    componentes++;
+                }
+            }
 
+            return componentes > 1;
+        }
+
+        //Metodo Visitar da busca em profundidade
+        private void Visitar(Vertice vertice)
+        {
+            vertice.cor = "CINZA";
+
+            foreach (var vert in vertice.adjacentes)
+            {
+                if (vert.Key.cor == "BRANCO")
+                {
+                    Visitar(vertice);
+                }
+            }
+
+            vertice.cor = "PRETO";               
+        }
+
+        //Sarah
+        public bool isEuleriano()
+        {
+            //Um grafo que não é conexo não pode ser Euleriano
+            if (!isConexo())
+            {
+                return false;
+            }
+
+            foreach (Vertice vertice in vertices)
+            {
+                //Verifica se todos os vertices possuem grau par, caso contráio o mesmo não é euleriano
+                if ((getGrau(vertice) % 2) != 0)
+                {
+                    return false;
+                }
+            }
+
+            //Significa que todos os vertices tem grau par 
+            return true;
+
+        }
+
+        //Sarah
+        public bool isUnicursal()
+        {            
+            //Um grafo que não é conexo não pode ser Unicursal
+            if (!isConexo())
+            {
+                return false;
+            }
+
+            int verticesGrauImpar = 0;
+
+            foreach (Vertice vertice in vertices)
+            {
+                //Verifica quantos vértices possuem grau par
+                if ((getGrau(vertice) % 2) != 0)
+                {
+                    verticesGrauImpar++;
+                }
+            }
+
+            //O grafo é Unicursal se 2k vértices possuem grau impar
+            if (verticesGrauImpar % 2 == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        //Sarah
+        public Grafo getComplementar()
+        {
+            Grafo grafo = new Grafo();
+
+            return grafo;
+
+        }
+
+        //Sarah
         public abstract Grafo getAGMPrim(Vertice v1);
+
 
         public abstract Grafo getAGMKruskal(Vertice v1);
 
+
         public abstract int getCutVertices();
+
+        public bool hasLoops()
+        {
+            return false;
+        }
 
 
 
--- Grafos/Grafos/Vertice.cs
+++ Grafos/Grafos/Vertice.cs
@@ -1,6 +1,29 @@
-﻿namespace Grafos
+﻿using System.Collections.Generic;
+
+namespace Grafos
 {
     public class Vertice
     {
+        public int id;
+
+        public string cor;
+        
+        //Lisa de vértices adjacentes (Contendo o vértice e o peso da aresta)
+        public Dictionary<Vertice, int> adjacentes;
+
+        public Vertice(int id)
+        {
+            this.id = id;
+            this.cor = "BRANCO";
+        }
+
+        public void addVertice (Vertice vertice, int peso)
+        {            
+            adjacentes.Add(vertice, peso);                     
+        }
+
+         
+
+
     }
 }
