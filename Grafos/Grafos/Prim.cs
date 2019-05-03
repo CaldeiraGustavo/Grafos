cc2b8bb53862fb852b4970725edd12bc2ace976d
Sarah <sarahrolandc@outlook.com>
2019-05-03 17:30:04 -0300

    Modificações classe Prim

--- Grafos/Grafos/Prim.cs
+++ Grafos/Grafos/Prim.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace Grafos
 {
     public static class Prim
-    {        
+    {
         private static List<Vertice> verticesConhecidos = new List<Vertice>();
         private static Vertice vOrigem;
 
         public static Grafo execute(Vertice origem, GrafoNaoDirigido grafo)
         {
-            GrafoNaoDirigido arvore = new GrafoNaoDirigido(grafo.gerarGrafoNulo());            
+            GrafoNaoDirigido arvore = new GrafoNaoDirigido(grafo.gerarGrafoNulo());
 
             vOrigem = origem;
 
@@ -25,7 +25,7 @@ namespace Grafos
                 arvore.adicionarAresta(vOrigem.id, aresta.vertice.id, aresta.peso);
 
                 //seleciona a aresta de menor peso
-                aresta = selecionarAresta(aresta.vertice);                
+                aresta = selecionarAresta(aresta.vertice);
             }
 
             return arvore;
@@ -51,27 +51,19 @@ namespace Grafos
                 {
                     if (aresta.peso <= peso && aresta.vertice.cor == "BRANCO")
                     {
-                        if (aresta.peso == peso) {
-                            
-                            if ((vOrigem.id + arestaSelecionada.vertice.id) <= (v.id + aresta.vertice.id)) {
-                                
-                                if ((vOrigem.id + arestaSelecionada.vertice.id) == (v.id + aresta.vertice.id)) {
-                                    
-                                }
-                                
-                            }
-                            else {
-                                peso = aresta.peso;
-                                arestaSelecionada = aresta;
-                                vOrigem = v;                        
+                        if (aresta.peso == peso)
+                        {
+                            Aresta arestaEscolhida = ArestasComMesmoPeso(arestaSelecionada, aresta, v);
+
+                            peso = arestaEscolhida.peso;
+                            arestaSelecionada = arestaEscolhida;
                         }
-                            
-                        }                        
-                        else {
+                        else
+                        {
                             peso = aresta.peso;
                             arestaSelecionada = aresta;
-                            vOrigem = v;                        
-                        }
+                            vOrigem = v;
+                        }                                  
                         
                     }
 
@@ -79,6 +71,36 @@ namespace Grafos
             }
 
             return arestaSelecionada;
+        }
+
+        private static Aresta ArestasComMesmoPeso(Aresta arestaAtual, Aresta arestaOutra, Vertice v)
+        {
+            if ((vOrigem.id + arestaAtual.vertice.id) >= (v.id + arestaOutra.vertice.id))
+            {                
+                vOrigem = v;
+                return arestaOutra;
+            }
+            else
+            {
+                if ((vOrigem.id + arestaAtual.vertice.id) == (v.id + arestaOutra.vertice.id))
+                {
+                    int indice1 = Math.Min(vOrigem.id, arestaAtual.vertice.id);
+                    int indice2 = Math.Min(v.id, arestaOutra.vertice.id);
+
+                    if (indice2 < indice1)
+                    {
+                        vOrigem = v;
+                        return arestaOutra;                        
+                    }
+                    else
+                    {
+                        return arestaAtual;
+                    }                       
+
+                }
+
+                return arestaAtual;
+            }
         }
     }
 }
